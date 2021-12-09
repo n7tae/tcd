0f60043d4d4c1503284f28193bbe1d28c6b0c2f2
Tom Early <n7tae@tearly.net>
2021-12-09 05:26:28 -0700

    reverse read in ReadDevice

--- Controller.cpp
+++ Controller.cpp
@@ -261,19 +261,19 @@ void CController::ReadAmbeDevices()
 		//wait for up to 40 ms to read anthing from all devices
 		if (rval > 0) {
 			// from the device file descriptor, we'll know if it's dstar or dmr
-			for (unsigned int i=0 ; i<dstar_device.size(); i++)
-			{
-				if (FD_ISSET(dstar_device[i]->GetFd(), &FdSet))
-				{
-					ReadDevice(dstar_device[i], EAmbeType::dstar);
-				}
-			}
 			for (unsigned int i=0 ; i<dmr_device.size(); i++)
 			{
 				if (FD_ISSET(dmr_device[i]->GetFd(), &FdSet))
 				{
 					ReadDevice(dmr_device[i], EAmbeType::dmr);
 					std::cout << "Read DMR device " << i << std::endl;
+				}
+			}
+			for (unsigned int i=0 ; i<dstar_device.size(); i++)
+			{
+				if (FD_ISSET(dstar_device[i]->GetFd(), &FdSet))
+				{
+					ReadDevice(dstar_device[i], EAmbeType::dstar);
 				}
 			}
 		}
